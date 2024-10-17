c09f500777a23bccecd56d7b3f7eb8c747a254d2
Sai Gumpalli <sai.gumpalli221@gmail.com>
2024-10-17 15:53:26 -0400

    integrated a model

--- backend/freakyAPI.py
+++ backend/freakyAPI.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, jsonify
+from PIL import Image
+import numpy as np
+import skin_cancer_detection as SCD
 
 app = Flask(__name__)
 app.config["UPLOAD_FOLDER"] = "downloads/"
@@ -7,7 +10,18 @@ app.config["UPLOAD_FOLDER"] = "downloads/"
 def home():
     if(request.method == 'POST'):
         file = request.files['file']
-        return jsonify({'message': 'melanoma bitch'})
+        inputimg = Image.open(file)
+        inputimg = inputimg.resize((28, 28))
+        img = np.array(inputimg).reshape(-1, 28, 28, 3)
+        result = SCD.model.predict(img)
+
+        result = result.tolist()
+        print(result)
+        max_prob = max(result[0])
+        class_ind = result[0].index(max_prob)
+        print(class_ind)
+        result = SCD.classes[class_ind]
+        return jsonify({'message': result})
     
 if __name__ == '__main__':
    app.run(port=5000)
