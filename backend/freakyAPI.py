b0ad08728ff250a478ff3d890e7eda65bcfb4b42
Sai Gumpalli <sai.gumpalli221@gmail.com>
2024-10-16 21:35:14 -0400

    simple endpoint

---
+++ backend/freakyAPI.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = "downloads/"
+
+@app.route('/freakyPics', methods=['POST'])
+def home():
+    if(request.method == 'POST'):
+        file = request.files['file']
+        return jsonify({'message': 'success'})
+    
+if __name__ == '__main__':
+   app.run(port=5000)
