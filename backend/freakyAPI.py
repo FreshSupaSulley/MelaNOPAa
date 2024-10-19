bef0417337e4d33d18eb5c5fd5f77468e8f47847
Sai Gumpalli <sai.gumpalli221@gmail.com>
2024-10-19 10:39:25 -0400

    freaky percentages

--- backend/freakyAPI.py
+++ backend/freakyAPI.py
@@ -16,12 +16,20 @@ def home():
         result = SCD.model.predict(img)
 
         result = result.tolist()
-        print(result)
+        print(result[0])
+        print(sum(result[0]))
         max_prob = max(result[0])
         class_ind = result[0].index(max_prob)
         print(class_ind)
-        result = SCD.classes[class_ind]
-        return jsonify({'message': result})
+        return jsonify({
+            "actinic keratoses and intraepithelial carcinomae(Cancer)": result[0][0],
+            "basal cell carcinoma(Cancer)": result[0][1],
+            "benign keratosis-like lesions(Non-Cancerous)": result[0][2],
+            "dermatofibroma(Non-Cancerous)": result[0][3],
+            "melanocytic nevi(Non-Cancerous)": result[0][4],
+            "pyogenic granulomas and hemorrhage(Can lead to cancer)": result[0][5],
+            "melanoma(Cancer)": result[0][6],
+        })
     
 if __name__ == '__main__':
    app.run(port=5000)
